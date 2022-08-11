79199cf955e6d36c592beccac5c9791ceb47733c
ygracoord <ygra.pl@gmail.com>
2022-08-11 20:37:15 +0300

    feat: complete reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+   if (n < 0) {
+       const num = -n;
+       return +num.toString().split('').reverse().join('')
+   } else return +n.toString().split('').reverse().join('')
 }
+
